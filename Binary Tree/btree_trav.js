023033cd06ed07e16de7383af965d74134a6f74b
Lujie Ma <xiaolinzzz2002@gmail.com>
2024-11-04 03:27:56 +1100

    inorder & preorder & postorder

--- Binary Tree/btree_trav.js
+++ Binary Tree/btree_trav.js
@@ -4,6 +4,23 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right);
 }
 
+// 前序：中左右
+function preorderTrav(root) {
+    let ans = [];
+
+    function inorder(node, list) {
+        if (!node) return;
+
+        list.push(node.val);
+        inorder(node.left, list);
+        inorder(node.right, list);
+    }
+
+    inorder(root, ans);
+
+    return ans;
+}
+
 // 中序：左中右
 function inorderTrav(root) {
     let ans = [];
@@ -21,6 +38,24 @@ function inorderTrav(root) {
     return ans;
 }
 
+//后序：左右中
+function postorderTrav(root) {
+    let ans = [];
+
+    function inorder(node, list) {
+        if (!node) return;
+
+        inorder(node.left, list);
+        inorder(node.right, list);
+        list.push(node.val);
+    }
+
+    inorder(root, ans);
+
+    return ans;
+}
+
+
 let root = new TreeNode(1);
 root.left = new TreeNode(2);
 root.right = new TreeNode(3);
@@ -36,3 +71,5 @@ root.left.right = new TreeNode(5);
 //  4   5
 
 console.log(inorderTrav(root));
+console.log(preorderTrav(root));
+console.log(postorderTrav(root));
